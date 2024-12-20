c229147f138f5cbef6d14ffd79e6c151ffafdad1
Gabriel Ribeiro <a104171@alunos.uminho.pt>
2024-12-20 23:03:44 +0000

    Problema 3 feito, mas com eventuais melhorias possíveis

--- P3.hs
+++ P3.hs
@@ -1,16 +1,30 @@
 import Cp
 import List
 
-createRow :: [Int] -> Int -> [Int]
-createRow l y = map (*y) l
+createRow :: Num a => [a] -> a -> [a]
+createRow l1 y = map (*y) l1
 
 
 --createMatrix [] _ = []
 --createMatrix _ [] = []
 --createMatrix l1 (y:ys) = createRow l1 y : createMatrix l1 ys
 
-createMatrix :: [Int] -> [Int] -> [[Int]]
+createMatrix :: Num a => [a] -> [a] -> [[a]]
 createMatrix l1 = anaList ((id -|- createRow l1 >< id) . outList)
 
-printMatrix :: [[Int]] -> String
-printMatrix = unlines . map (unwords . map show)
+--Tentar juntar esta com a de cima no mesmo anamorfismo // ou transformar esta num cata/anamorfismo // usar os combinadores de CP ?
+diagonals :: Num a => [[a]] -> [[a]]
+diagonals [] = []
+diagonals [row] = map (: []) row
+diagonals (row : rows) = extendDiagonals row (diagonals rows)
+  where
+    extendDiagonals row diags = zipWith (:) row ([] : diags) ++ drop (length row - 1) diags
+
+--Também pode ser só assim, mas não usa combinadores, não sei qual a mais apreciada
+--sumDiagonals = map sum
+sumDiagonals :: Num a => [[a]] -> [a]
+sumDiagonals = cataList (either nil (cons . (sum >< id)))
+
+--sumDiagonals = map sum
+convolve :: Num a => [a] -> [a] -> [a]
+convolve l1 = sumDiagonals . diagonals . createMatrix l1
