f1502417b0e1420bef401a813faeac7ff5e6e909
Gabriel Ribeiro <a104171@alunos.uminho.pt>
2024-12-22 20:00:47 +0000

    Problema 2 - parte 1 (necessita de revisão)

---
+++ P2.hs
@@ -0,0 +1,21 @@
+import Cp
+import List
+
+out 0 = i1 ()
+out 1 = i1 ()
+out (-1) = i1 ()
+out n = i2 (abs n)
+
+isPrime :: Int -> Bool
+isPrime n | n < 2     = False
+          | otherwise = null [x | x <- [2..isqrt n], mod n x == 0]
+    where isqrt = floor . sqrt . fromIntegral
+
+nextPrimeFactor :: Int -> Int
+nextPrimeFactor n = head [x | x <- [2..n], isPrime x && mod n x == 0]
+
+divide :: Int -> Int
+divide n = div n (nextPrimeFactor n)
+
+primes :: Int -> [Int]
+primes = anaList ((id -|- split nextPrimeFactor divide) . out)
