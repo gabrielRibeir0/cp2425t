51474f620cfcdfe32b25a7d8a05d4e3325825d98
Gabriel Ribeiro <a104171@alunos.uminho.pt>
2025-01-05 12:40:55 +0000

    mais operadores de CP no problema 2

--- P2.hs
+++ P2.hs
@@ -1,21 +1,28 @@
 import Cp
 import List
 
-out 0 = i1 ()
-out 1 = i1 ()
-out (-1) = i1 ()
-out n = i2 (abs n)
+outPrimes 0 = i1 ()
+outPrimes 1 = i1 ()
+outPrimes (-1) = i1 ()
+outPrimes n = i2 (abs n)
+
+isPrimeAux :: Int -> Bool
+isPrimeAux n = (either true false . outList) [x | x <- [2..isqrt n], mod n x == 0]
+    where isqrt = floor . sqrt . fromIntegral
 
 isPrime :: Int -> Bool
-isPrime n | n < 2     = False
-          | otherwise = null [x | x <- [2..isqrt n], mod n x == 0]
-    where isqrt = floor . sqrt . fromIntegral
+isPrime = either false isPrimeAux . outPrimes
 
 nextPrimeFactor :: Int -> Int
 nextPrimeFactor n = head [x | x <- 2:[3,5..n], mod n x == 0 && isPrime x]
 
-divide :: Int -> Int
-divide n = div n (nextPrimeFactor n)
+--Explorar mais isto para as funções de criar listas acima (não sei se é mesmo preciso)
+a :: Integer -> Integer  -> Bool
+a x = (== 0) . flip mod x
+
+--Ver melhor como aap funciona, se não se perceber, deixar estar o divide
+--divide :: Int -> Int
+--divide n = div n (nextPrimeFactor n)
 
 primes :: Int -> [Int]
-primes = anaList ((id -|- split nextPrimeFactor divide) . out)
+primes = anaList ((id -|- split nextPrimeFactor (aap div nextPrimeFactor)) . outPrimes)
