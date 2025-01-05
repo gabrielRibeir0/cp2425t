3ab5001bb16160257fc82d89c10e833de6e94471
Gabriel Ribeiro <a104171@alunos.uminho.pt>
2025-01-05 20:17:56 +0000

    problema 2

--- P2.hs
+++ P2.hs
@@ -6,23 +6,15 @@ outPrimes 1 = i1 ()
 outPrimes (-1) = i1 ()
 outPrimes n = i2 (abs n)
 
-isPrimeAux :: Int -> Bool
-isPrimeAux n = (either true false . outList) [x | x <- [2..isqrt n], mod n x == 0]
+divisorsList :: Int -> [Int]
+divisorsList n = [x | x <- [2..isqrt n], mod n x == 0]
     where isqrt = floor . sqrt . fromIntegral
 
 isPrime :: Int -> Bool
-isPrime = either false isPrimeAux . outPrimes
+isPrime = either false (either true false . outList . divisorsList) . outPrimes
 
 nextPrimeFactor :: Int -> Int
 nextPrimeFactor n = head [x | x <- 2:[3,5..n], mod n x == 0 && isPrime x]
-
---Explorar mais isto para as funções de criar listas acima (não sei se é mesmo preciso)
-a :: Integer -> Integer  -> Bool
-a x = (== 0) . flip mod x
-
---Ver melhor como aap funciona, se não se perceber, deixar estar o divide
---divide :: Int -> Int
---divide n = div n (nextPrimeFactor n)
 
 primes :: Int -> [Int]
 primes = anaList ((id -|- split nextPrimeFactor (aap div nextPrimeFactor)) . outPrimes)
