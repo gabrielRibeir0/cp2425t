a9de65de63cb096de75584fa098d80c275ae7e36
JuliaBughi <ju.bughi@gmail.com>
2025-01-07 17:12:39 +0000

    correção de erro na 1

--- P1.hs
+++ P1.hs
@@ -1,20 +1,18 @@
 import Cp
 import List
+import Data.Maybe
 
+--ana
 g :: [Int] -> Either () ((Int, [Int]), [Int])
---g [] = i1 ()
---g ys = i2 ((head xs, xs), tail xs)
-  --  where xs = iSort ys
-g = (id -|- split (split head id) tail . cons) . outList . iSort
+g = (id -|-  (split (split head id) tail . cons)) . outList . iSort 
 
+--funções auxiliares 
+pp1 = uncurry (/=) . split (p1.p2) (const 0) 
+pp2 = uncurry (<=) . split (p1.p2) (length.p2.p2)
+
+--cata
 f :: Either () ((Int, [Int]), (Int, [Int])) -> (Int, [Int])
---f (Left ()) = (0,[])
---f (Right ((i,xs), prev)) = if i >= length xs 
-  --                         then (i,xs)
-    --                       else prev
-
-f = either (const (0,[])) (cond (uncurry (>=) . split (p1.p1) (length.p2.p1)) p1 p2)
-
+f = either (split (const 0) nil) (cond (uncurry (&&) . split pp1 pp2 ) p2 p1)
 
 hIndex :: [Int] -> (Int,[Int])
 hIndex = hyloList f g
