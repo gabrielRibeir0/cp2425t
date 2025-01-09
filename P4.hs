1f0db6cf482e34628cf227f2b80e901a7dda13fb
Luís Cunha <luispintocunha@hotmail.com>
2025-01-09 23:05:01 +0000

    algumas cenas do 4

--- P4.hs
+++ P4.hs
@@ -1,4 +1,5 @@
 import Cp 
+import St 
 
 
 data Op = ITE 
@@ -15,14 +16,16 @@ data Expr b a = V a
 
 inExpr = either V (either N (uncurry T))
 
+
 outExpr(V a) = i1 a
 outExpr(N b) = i1 b
-outExpr(T op l) = i2(op,l)
+outExpr(T op l) = i2 (i2 (op,l))
+
 
 baseExpr g h f = g -|- (h -|- id >< map f)
 
- 
-recExpr f = baseExpr id id f 
+
+recExpr f = baseExpr id id  f
 
 cataExpr g = g . recExpr (cataExpr g) . outExpr
 
@@ -30,22 +33,35 @@ anaExpr f = inExpr . recExpr (anaExpr f) . f
 
 hyloExpr h g = cataExpr h . anaExpr g 
 
+instance Functor (Expr b) where
+    fmap f (V x) = V (f x)
+    fmap _ (N n) = N n
+    fmap f (T op exprs) = T op (map (fmap f) exprs)
+
+instance Applicative (Expr b) where
+    pure = return
+    (<*>) = aap 
+
+instance Monad (Expr b) where
+    return = V
+    (V x) >>= f = f x
+    (N n) >>= _ = N n
+    (T op exprs) >>= f = T op (map (>>= f) exprs)
+
+--catamorfismo monadico
+mcataExpr :: Monad m ⇒ (a + (b + (Op, m [c])) → m c) → Expr b a → m c
+mcataExpr g = 
+
+
 -- Maps: Monad: Let Expressions:
 --let_exp :: (Num c) ⇒ (a → Expr c b) → Expr c a → Expr c b
 --let_exp 
 
---catamorfismo monadico
---mcataExpr :: Monad m ⇒ (a + (b + (Op, m [c])) → m c) → Expr b a → m c
---mcataExpr g
+
 
 --avaliacao de expressoes
 --evaluate :: (Num a, Ord a) ⇒ Expr a b → Maybe a
 --evaluate
-
-
-
-
-
 
 
 
